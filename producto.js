9a483f670245570767c00e1e779591997721ae34
Alberto García <agarciai03@iesalbarregas.es>
2025-10-08 17:24:45 +0200

    creación producto.js
    se crea todo lo requerido para que funcione

---
+++ producto.js
@@ -0,0 +1,31 @@
+class Producto {
+    constructor(nombre, precio, rareza, tipo, bonus) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.rareza = rareza;
+        this.tipo = tipo;
+        this.bonus = bonus;
+    }
+
+
+    //presentación que describa al objeto
+    presentar() {
+        const precioFormateado = this.formatearPrecio();
+        return `El producto ${this.nombre} es de tipo ${this.tipo}, tiene una rareza de ${this.rareza}, un precio de ${precioFormateado} y otorga un bonus de ${JSON.stringify(this.bonus)}.`;
+    }
+
+    //formateando su precio en € (los objetos guardan su precio sin decimales por ejemplo 950, esto se formateara a 9,50€)
+    formatearPrecio() {
+        return (this.precio / 100).toFixed(2) + "€";
+    }
+
+
+    //Aplicar un descuento: Le llega un valor y devuelve una copia de ese producto modificado.
+    aplicarDescuento(porcentaje) {
+        const copia = structuredClone(this);
+        copia.precio = Math.round(this.precio * (1 - porcentaje / 100));
+        return copia;
+    }
+}
+
+export { Producto };
