7c82409dc9de3a2fd33145cf32e2c8887331cff4
Juanfrina <jfco11092024@outlook.com>
2025-10-08 16:34:29 +0200

    jugadores

---
+++ Jugadores.js
@@ -0,0 +1,21 @@
+// clase Jugador con atributos nombre, puntos, vidaMaxima e inventario
+export class Jugadores {
+    nombre;
+    puntos = 0;
+    vidaMaxima = 100;
+    inventario = [];
+
+    constructor(nombre, vidaMaxima) {
+        this.nombre = nombre;
+        if (vidaMaxima) this.vidaMaxima = vidaMaxima;
+    }
+
+    //añadir objetos al inventario
+    añadirObjeto(objeto) {
+        const objetoClone = { ...objeto };
+        this.inventario.push(objetoClone);
+    }
+
+    //sumar puntos despues de la batalla
+    
+}
