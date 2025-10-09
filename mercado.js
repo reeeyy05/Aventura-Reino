90a3217003220b2b6181a8b74b915692780940c6
ElReeeyy <areyt01@iesalbarregas.es>
2025-10-09 18:04:46 +0200

    Metodo aplicar descuento
    Cambios en el metodo

--- mercado.js
+++ mercado.js
@@ -1,29 +1,26 @@
-import Producto from './producto.js';
+import p from '/producto.js';
 
-export { Mercado };
-
-class Mercado {
-    listaProductos = [];
-
-    constructor() {
-        this.listaProductos.push(new Producto("Espada Basica", 500, "Comun", "arma", "aumenta ataque"));
-        this.listaProductos.push(new Producto("Armadura Ligera", 750, "Rara", "armadura", "aumenta defensa"));
-        this.listaProductos.push(new Producto("Pocion de vida", 200, "Comun", "consumible", "cura vida"));
-        this.listaProductos.push(new Producto("Granada", 250, "Raro", "consumible", "mata al enemigo"));
-    }
+export class Mercado {
+    listaProductos = [
+        new p("Espada Basica", 500, "Comun", "arma", "aumenta ataque"),
+        new p("Armadura Ligera", 750, "Rara", "armadura", "aumenta defensa"),
+        new p("Pocion de vida", 200, "Comun", "consumible", "cura vida"),
+        new p("Granada", 250, "Raro", "consumible", "mata al enemigo")
+    ];
 
     // Filtrar productos por una rareza dada
     filtrarPorRareza(rareza) {
-        return this.listaProductos.filter(producto => producto.rareza === rareza);
+        return this.listaProductos.filter(Producto => Producto.rareza === rareza);
     }
 
     // Aplicar un descuento a los productos que sean de un tipo o rareza concreta
     aplicarDescuento(tipoRareza) {
-        this.listaProductos.forEach(producto => {
-            if (producto.tipo === tipoRareza) {
-                producto.precio -= 50;
+        for (let i = 0; i < p.length; i++) {
+            if (p[i].tipo === tipoRareza) {
+                p[i].precio -= 50;
             }
-        });
+            
+        }
     }
 
     // Buscar un producto por su nombre
@@ -33,11 +30,6 @@ class Mercado {
 
     // Mostrar un producto (se llamará al método que tiene este producto)
     mostrarProducto(nombre) {
-        const producto = this.buscarProducto(nombre);
-        if (producto) {
-            producto.mostrarInfo();
-        } else {
-            console.log("Producto no encontrado");
-        }
+        return p.presentar();
     }
 }
