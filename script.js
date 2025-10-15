b881ad7e2356e05e2de85f1cfce2c2384abb0240
Juanfrina <jfco11092024@outlook.com>
2025-10-15 18:39:12 +0200

    crear Jugadores

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //importar la clase Jugadores
 import { Jugadores } from './Jugadores.js';
+
+// bienvenida
+console.log("🏰Bienvenidos a la Aventura del Reino🏰");
+
+
+//Crear 3 jugadores
+const jugador1 = new Jugadores('Paco');
+const jugador2 = new Jugadores('Juan');
+const jugador3 = new Jugadores('Gustavo');
+console.log("👤Jugadores creados: ", jugador1.nombre, jugador2.nombre, jugador3.nombre);
+
+
