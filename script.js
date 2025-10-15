c649f4704e00de503bb60d171f54c919acac8016
joseantoio1234 <jagonzalezm38@educarex.es>
2025-10-15 17:49:04 +0200

    import de clase enemigo

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//importar la clase Jugadores
-import { Jugadores } from './Jugadores.js';
+// Importar la clase enemigo
+import { enemigos, JefeFinal } from './enemigos.js';
+
+const enemigos = [
+  new enemigos('Goblin', 6, 30),
+  new enemigos('Lobo', 9, 42),
+  new enemigos('Bandido', 12, 55), 
+  new JefeFinal('Dragon', 28, 140, 'Aliento de Fuego', 2.0),
+];
+  
+console.log('\n👿 Enemigos:');
+enemigos.forEach(e => console.log(`  - ${e.mostrarEnemigos()}`));
